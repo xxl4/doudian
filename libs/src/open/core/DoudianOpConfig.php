e87fed6e3a26ee0d7a1ec4e6bf9c70f70ab1442e
StevenLiu <nice.lizhi@gmail.com>
2023-10-11 18:38:29 +0800

    add libs

---
+++ libs/src/open/core/DoudianOpConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+class DoudianOpConfig
+{
+    public $appKey = "";
+    public $appSecret="";
+    public $httpConnectTimeout = 1000;
+    public $httpReadTimeout = 5000;
+    public $openRequestUrl = "https://openapi-fxg.jinritemai.com";
+}
